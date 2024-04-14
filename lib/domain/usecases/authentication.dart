cf7a23d8b88e86d2a9ad2f9b566e74f95527f9b7
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-04-14 16:09:24 -0300

    refactor: changes authentication usecase to received a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,10 +1,15 @@
-import 'package:flutter/material.dart';
-
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    @required String email,
-    @required String password
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password
   });
 }
