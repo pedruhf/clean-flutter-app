8aa08990440b3d3b762fc1d178d08600cd85ced8
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-05-04 18:48:34 -0300

    test: ensure that post will be called with correct values

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,52 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class ClientSpy extends Mock implements Client {
+  When mockPostCall() => when(() => this.post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPost(int statusCode, { String body = '{"any_key":"any_value"}' }) => mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+  }) async {
+    await client.post(Uri.parse(url));
+  }
+}
+
+void main() {
+  late ClientSpy client;
+  late String url;
+  late HttpAdapter sut;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
+  });
+
+  group('post', () {
+    setUp(() {
+      client.mockPost(200);
+    });
+
+    test('should call post with correct values', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(() => client.post(
+        Uri.parse(url),
+      ));
+    });
+  });
+}
