6c41ec712e445ff0c87a3f09175c070863f72f14
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-05-15 19:09:54 -0300

    feat: ensure StreamLoginPresenter emits error if validation fails

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -6,25 +8,49 @@ abstract class Validation {
   validate({ required String field, required String value });
 }
 
+class LoginState {
+  String emailError = '';
+}
+
 class StreamLoginPresenter {
-  Validation validation;
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  final _state = LoginState();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({ required this.validation });
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
 class ValidationSpy extends Mock implements Validation {}
 
 void main() {
-  test('should call Validation with correct email', () {
-    final validation = ValidationSpy();
-    final sut = StreamLoginPresenter(validation: validation);
-    final email = faker.internet.email();
+  late StreamLoginPresenter sut;
+  late Validation validation;
+  late String email;
 
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+  });
+
+  test('should call Validation with correct email', () {
     sut.validateEmail(email);
 
     verify(() => validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('should emit email error if validation fails', () {
+    when(() => validation.validate(field: any(named: 'field'), value: any(named: 'value'))).thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
