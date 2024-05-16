ad1720cc026727a6317d9fd87e607a44d19d8486
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-05-15 22:01:57 -0300

    feat: ensure StreamLoginPresenter emits password error if validation fails

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -66,4 +66,13 @@ void main() {
 
     verify(() => validation.validate(field: 'password', value: password)).called(1);
   });
+
+  test('should emit password error if validation fails', () {
+    mockValidation(value: 'error');
+
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, 'error')));
+
+    sut.validatePassword(email);
+    sut.validatePassword(email);
+  });
 }
