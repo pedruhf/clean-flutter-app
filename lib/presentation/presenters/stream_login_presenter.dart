2063746c64c5d97ae5d908287bcc27acbe4a35d2
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-05-15 21:57:37 -0300

    feat: ensure StreamLoginPresenter call Validation with correct password

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -20,8 +20,14 @@ class StreamLoginPresenter {
       _controller.stream.map((state) => state.isFormValid).distinct();
 
   StreamLoginPresenter({required this.validation});
+
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
+  }
+
+  void validatePassword(String password) {
+    validation.validate(field: 'password', value: password);
+    
   }
 }
