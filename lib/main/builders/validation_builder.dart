332937f993b03fd3861f296281176d6a2c5ffe4a
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-06-22 13:17:26 -0300

    feat: adds ValidationBuilder

---
+++ lib/main/builders/validation_builder.dart
@@ -0,0 +1,29 @@
+import 'package:clean_flutter_app/validation/validators/email_validation.dart';
+import 'package:clean_flutter_app/validation/validators/protocols/field_validation.dart';
+import 'package:clean_flutter_app/validation/validators/required_field_validation.dart';
+
+class ValidationBuilder {
+  static ValidationBuilder _instance = ValidationBuilder();
+  String fieldName = '';
+  List<FieldValidation> validations = [];
+
+  static ValidationBuilder field(String fieldName) {
+    _instance = ValidationBuilder();
+    _instance.fieldName = fieldName;
+    return _instance;
+  }
+
+  ValidationBuilder required() {
+    validations.add(RequiredFieldValidation(fieldName));
+    return this;
+  }
+
+  ValidationBuilder email() {
+    validations.add(EmailValidation(fieldName));
+    return this;
+  }
+
+  List<FieldValidation> build() {
+    return validations;
+  }
+}
--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,5 @@
+import 'package:clean_flutter_app/main/builders/validation_builder.dart';
+
 import '../../../../presentation/protocols/validation.dart';
 import '../../../../validation/validators/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
@@ -8,8 +10,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> getLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
