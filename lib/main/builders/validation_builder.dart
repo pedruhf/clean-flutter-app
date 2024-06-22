15e8754f2eecb562e23552cbb3e95027495c118d
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-06-22 13:46:32 -0300

    refactor: transforms ValidationBuilder constructor to private

--- lib/main/builders/validation_builder.dart
+++ lib/main/builders/validation_builder.dart
@@ -3,12 +3,14 @@ import 'package:clean_flutter_app/validation/validators/protocols/field_validati
 import 'package:clean_flutter_app/validation/validators/required_field_validation.dart';
 
 class ValidationBuilder {
-  static ValidationBuilder _instance = ValidationBuilder();
+  static ValidationBuilder _instance = ValidationBuilder._();
   String fieldName = '';
   List<FieldValidation> validations = [];
 
+  ValidationBuilder._();
+
   static ValidationBuilder field(String fieldName) {
-    _instance = ValidationBuilder();
+    _instance = ValidationBuilder._();
     _instance.fieldName = fieldName;
     return _instance;
   }
