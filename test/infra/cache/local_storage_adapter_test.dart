6d322bbe4145606d5cc725e368453fb1cb4bd40a
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-06-27 23:14:02 -0300

    refactor: move LocalStorageAdapter to production file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,21 +1,8 @@
-import 'package:clean_flutter_app/domain/helpers/helpers.dart';
+import 'package:clean_flutter_app/infra/cache/cache.dart';
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-import 'package:clean_flutter_app/data/cache/save_secure_cache_storage.dart';
-
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-
-  LocalStorageAdapter({required this.secureStorage});
-
-  @override
-  Future<void> saveSecure({required String key, required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
