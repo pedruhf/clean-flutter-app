4c11bf692c4215e71a4ae486a9ccf296bcb15c1e
Pedro Freitas <pedroh.ufrn@gmail.com>
2024-06-30 10:05:36 -0300

    feat: ensure GetxSplashPresenter go to surveys page on success

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,5 +1,7 @@
+import 'package:clean_flutter_app/domain/entities/entities.dart';
 import 'package:clean_flutter_app/domain/usecases/usecases.dart';
 import 'package:clean_flutter_app/ui/pages/pages.dart';
+import 'package:faker/faker.dart';
 import 'package:get/get.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -9,34 +11,46 @@ class GetxSplashPresenter implements SplashPresenter {
 
   final RxString _navigateTo = RxString('');
 
-  GetxSplashPresenter({ required this.loadCurrentAccount });
+  GetxSplashPresenter({required this.loadCurrentAccount});
 
   @override
-  Stream<String> get navigateToStream => _navigateTo.stream; 
+  Stream<String> get navigateToStream => _navigateTo.stream;
 
   @override
   Future<void> checkAccount() async {
     await loadCurrentAccount.load();
+    _navigateTo.value = '/surveys';
   }
 }
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
 void main() {
+  late String token;
   late LoadCurrentAccountSpy loadCurrentAccount;
   late GetxSplashPresenter sut;
 
+  When mockLoadCurrentAccount() {
+    return when(() => loadCurrentAccount.load());
+  }
+
   setUp(() {
+    token = faker.guid.guid();
     loadCurrentAccount = LoadCurrentAccountSpy();
-    when(() => loadCurrentAccount.load()).thenAnswer((_) async => null);
     sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+
+    mockLoadCurrentAccount().thenAnswer((_) async => AccountEntity(token));
   });
 
   test('should call LoadCurrentAccount', () async {
-    
-
     await sut.checkAccount();
 
     verify(() => loadCurrentAccount.load()).called(1);
+  });
+
+  test('should go to surveys page on success', () async {
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/surveys')));  
+  
+    await sut.checkAccount();
   });
 }
